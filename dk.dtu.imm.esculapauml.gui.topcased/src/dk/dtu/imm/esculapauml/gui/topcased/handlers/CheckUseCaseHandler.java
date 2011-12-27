57c809b84508ac267f2e3f1c1baa254981d5555e
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2011-12-27 18:55:05 +0100

    show error markers (not completed)

--- dk.dtu.imm.esculapauml.gui.topcased/src/dk/dtu/imm/esculapauml/gui/topcased/handlers/CheckUseCaseHandler.java
+++ dk.dtu.imm.esculapauml.gui.topcased/src/dk/dtu/imm/esculapauml/gui/topcased/handlers/CheckUseCaseHandler.java
@@ -13,14 +13,21 @@
 
 package dk.dtu.imm.esculapauml.gui.topcased.handlers;
 
+
 import java.util.List;
+
 import dk.dtu.imm.esculapauml.gui.topcased.utils.GuiUtils;
 import dk.dtu.imm.esculapauml.core.ConsistencyCheckingService;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.ui.util.EditUIMarkerHelper;
+import org.eclipse.emf.common.ui.MarkerHelper;
+import org.eclipse.emf.common.util.Diagnostic;
 
 /**
  * command to trigger use case consistency checking on selected interaction
@@ -37,6 +44,22 @@ public class CheckUseCaseHandler extends AbstractHandler {
 		} else {
 			throw new IllegalArgumentException("Passed argument is empty or of a wrong type (required EMF model element argument)");
 		}
+
+		MarkerHelper mh = new EditUIMarkerHelper();
+		try {
+			Diagnostic diag = ConsistencyCheckingService.getInstance().getDiagnostics();
+			mh.deleteMarkers(GuiUtils.getSelectedResource(event));
+			mh.createMarkers(diag);
+			//org.topcased.validation.core.MarkerUtil.
+		
+
+
+			
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 		
 	}
