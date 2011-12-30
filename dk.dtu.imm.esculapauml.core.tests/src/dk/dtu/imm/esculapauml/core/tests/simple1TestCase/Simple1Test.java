cc43ba3ed12df45c6985c594271b44686ee9e048
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2011-12-30 19:22:50 +0100

    more tests

--- dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/tests/simple1TestCase/Simple1Test.java
+++ dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/tests/simple1TestCase/Simple1Test.java
@@ -16,21 +16,27 @@ import static org.junit.Assert.*;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.uml2.uml.Interaction;
+import org.eclipse.uml2.uml.Lifeline;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.UMLFactory;
 import org.junit.Test;
 
 import dk.dtu.imm.esculapauml.core.checkers.UseCaseChecker;
 import dk.dtu.imm.esculapauml.core.tests.utils.TestUtils;
 
 /**
- * Simple scenario containing one interaction without messages / just structural check
+ * Simple scenario containing one interaction without messages / just structural
+ * check
+ * 
  * @author Piotr. J. Puczynski (piotr.puczynski)
- *
+ * 
  */
 public class Simple1Test {
 
+	private Resource model = TestUtils.getUMLResource("Simple1.uml");
+
 	@Test
 	public void emptyInteraction() {
-		Resource model = TestUtils.getUMLResource("Simple1.uml");
 		Interaction interaction = TestUtils.getInteraction(model, "UseCase1Detail");
 		assertNotNull(interaction);
 		UseCaseChecker checker = new UseCaseChecker(interaction);
@@ -38,6 +44,41 @@ public class Simple1Test {
 		Diagnostic diagnostics = checker.getDiagnostics();
 		// no errors
 		assertEquals(Diagnostic.OK, diagnostics.getSeverity());
+	}
+
+	@Test
+	public void interactionWithLifelineWithoutRepresentant() {
+		Interaction interaction = TestUtils.getInteraction(model, "UseCase1Detail");
+		assertNotNull(interaction);
+
+		// we add one lifeline without representant to interaction
+		String lifelineName = "EmptyClass";
+		Lifeline empty = interaction.createLifeline(lifelineName);
+		UseCaseChecker checker = new UseCaseChecker(interaction);
+		checker.check();
+		Diagnostic diagnostics = checker.getDiagnostics();
+		// there is an error
+		assertEquals(Diagnostic.ERROR, diagnostics.getSeverity());
+		// there is one error
+		assertEquals(1, TestUtils.getDiagnosticErrorsAndWarnings(diagnostics).size());
+		// an error is...
+		assertTrue(TestUtils.diagnosticExists(diagnostics, Diagnostic.ERROR, "The Lifeline " + lifelineName + " has no representant.",
+				empty));
+
+		// let create a connector element that won't point to any representant
+		Property prop = interaction.createOwnedAttribute("MyProperty", UMLFactory.eINSTANCE.createProperty().getType());
+		empty.setRepresents(prop);
+		checker = new UseCaseChecker(interaction);
+		checker.check();
+		diagnostics = checker.getDiagnostics();
+		// there is an error
+		assertEquals(Diagnostic.ERROR, diagnostics.getSeverity());
+		// there is one error
+		assertEquals(1, TestUtils.getDiagnosticErrorsAndWarnings(diagnostics).size());
+		// an error is...
+		assertTrue(TestUtils.diagnosticExists(diagnostics, Diagnostic.ERROR, "The Lifeline " + lifelineName + " has no representant set to any type.",
+				empty));
+
 	}
 
 }
--- dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/tests/utils/TestUtils.java
+++ dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/tests/utils/TestUtils.java
@@ -11,6 +11,9 @@
  ****************************************************************************/
 package dk.dtu.imm.esculapauml.core.tests.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -20,16 +23,19 @@ import org.eclipse.uml2.common.util.UML2Util;
 import org.eclipse.uml2.uml.Interaction;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.resource.UMLResource;
+import org.eclipse.emf.common.util.Diagnostic;
 
 /**
  * Testing methods help
+ * 
  * @author Piotr. J. Puczynski (piotr.puczynski)
  * 
  */
 public class TestUtils {
-	
+
 	/**
 	 * Loads resource file as UML2 model
+	 * 
 	 * @param modelFileName
 	 * @return resource
 	 */
@@ -43,9 +49,10 @@ public class TestUtils {
 
 		return resourceSet.getResource(uri, true);
 	}
-	
+
 	/**
 	 * Finds an interaction in UML2 model based on given name
+	 * 
 	 * @param model
 	 * @param interactionName
 	 * @return Interaction element
@@ -54,12 +61,57 @@ public class TestUtils {
 		Interaction umlInteraction = null;
 		umlInteraction = (Interaction) UML2Util.findEObject(model.getAllContents(), new UML2Util.EObjectMatcher() {
 			public boolean matches(EObject eObject) {
-				if(eObject.eClass().getName().equals("Interaction")) {
+				if (eObject.eClass().getName().equals("Interaction")) {
 					return ((Interaction) eObject).getName().equals(interactionName);
 				}
 				return false;
 			}
 		});
 		return umlInteraction;
+	}
+
+	/**
+	 * Gets the diagnostic leafs with errors or warnings as a list
+	 * @param diagnostic
+	 * @return
+	 */
+	public static List<Diagnostic> getDiagnosticErrorsAndWarnings(Diagnostic diagnostic) {
+		ArrayList<Diagnostic> result = new ArrayList<Diagnostic>();
+
+		if (!diagnostic.getChildren().isEmpty()) {
+			for (Diagnostic childDiagnostic : diagnostic.getChildren()) {
+				result.addAll(getDiagnosticErrorsAndWarnings(childDiagnostic));
+			}
+		} else {
+			if (diagnostic.getSeverity() > Diagnostic.OK) {
+				result.add(diagnostic);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Checks if there is an error given in parameters in diagnostic
+	 * @param diagnostic
+	 * @param severity
+	 * @param message
+	 * @param objects
+	 * @return
+	 */
+	public static boolean diagnosticExists(Diagnostic diagnostic, int severity, String message, Object... objects) {
+		ArrayList<Object> diagobj = null;
+		if (null != objects) {
+			diagobj = new ArrayList<Object>();
+		}
+		for (Object obj : objects) {
+			diagobj.add(obj);
+		}
+		List<Diagnostic> diagnostics = getDiagnosticErrorsAndWarnings(diagnostic);
+		for (Diagnostic diag : diagnostics) {
+			if ((diag.getSeverity() == severity) && (diag.getMessage().equals(message)) && (diag.getData().equals(diagobj))) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
