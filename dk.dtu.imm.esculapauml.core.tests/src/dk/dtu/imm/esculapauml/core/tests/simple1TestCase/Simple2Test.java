afec7ccb11650493e73840da92e365891f91ffcb
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2012-01-19 18:03:13 +0100

    Simple2 test

---
+++ dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/tests/simple1TestCase/Simple2Test.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright (c) 2011, 2012 Piotr J. Puczynski (DTU Informatics).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Piotr J. Puczynski (DTU Informatics) - initial API and implementation 
+ *    
+ ****************************************************************************/
+package dk.dtu.imm.esculapauml.core.tests.simple1TestCase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.uml2.uml.Interaction;
+import org.junit.Test;
+
+import dk.dtu.imm.esculapauml.core.checkers.UseCaseChecker;
+import dk.dtu.imm.esculapauml.core.tests.utils.TestUtils;
+
+/**
+ * 
+ * Simple scenario containing one interaction with one message / just structural
+ * check
+ * @author Piotr J. Puczynski
+ *
+ */
+public class Simple2Test {
+	private Resource model = TestUtils.getUMLResource("Simple2.uml");
+	
+	@Test
+	public void okInteraction() {
+		Interaction interaction = TestUtils.getInteraction(model, "UseCase1Detail");
+		assertNotNull(interaction);
+		UseCaseChecker checker = new UseCaseChecker(interaction);
+		checker.check();
+		Diagnostic diagnostics = checker.getDiagnostics();
+		// no errors
+		assertEquals(Diagnostic.OK, diagnostics.getSeverity());
+	}
+}
