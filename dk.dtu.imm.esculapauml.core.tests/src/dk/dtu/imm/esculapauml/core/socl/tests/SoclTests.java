d8c6af95676a5ce09b98c889ba12af8acaa7d78a
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2012-01-22 17:09:13 +0100

    Added socl to core

---
+++ dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/socl/tests/SoclTests.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Copyright (c) 2011, 2012 Piotr J. Puczynski (DTU Informatics).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Piotr J. Puczynski (DTU Informatics) - initial API and implementation 
+ *    
+ ****************************************************************************/
+package dk.dtu.imm.esculapauml.core.socl.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import dk.dtu.imm.esculapauml.core.socl.parser.*;
+
+/**
+ * @author Piotr J. Puczynski
+ *
+ */
+public class SoclTests {
+
+	@Test
+	public void test() {
+		SOCLParser socl = new SOCLParser("");
+	}
+
+}
