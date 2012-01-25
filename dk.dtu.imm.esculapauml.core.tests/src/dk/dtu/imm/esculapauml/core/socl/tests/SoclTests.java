0e4a840872deb45d02ae07e941f284af2068ca36
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2012-01-25 13:40:47 +0100

    fixed logical constant in parser

--- dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/socl/tests/SoclTests.java
+++ dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/socl/tests/SoclTests.java
@@ -87,5 +87,65 @@ public class SoclTests {
 		assertEquals("member2", member2.jjtGetValue());
 		
 	}
+	
+	@Test
+	public void parseMemberFunction() throws ParseException {
+		SOCLParser parser = new SOCLParser("obj1.member2(123, \"str\")");
+		
+
+		SOCLNode root = parser.parse();
+		//root.dump("");
+		
+		assertNotNull(root);
+		assertEquals(1, root.jjtGetNumChildren());
+		
+		SOCLNode member = (SOCLNode) root.jjtGetChild(0);
+		assertEquals(SOCLParserTreeConstants.JJTMEMEBEROP, member.getId());
+		assertEquals(2, member.jjtGetNumChildren());
+		
+		SOCLNode obj1 = (SOCLNode) member.jjtGetChild(0);
+		SOCLNode member2 = (SOCLNode) member.jjtGetChild(1);
+		
+		assertEquals(SOCLParserTreeConstants.JJTIDENTIFIER, obj1.getId());
+		assertEquals(SOCLParserTreeConstants.JJTCALL, member2.getId());
+		assertEquals("obj1", obj1.jjtGetValue());
+		SOCLNode ident = (SOCLNode) member2.jjtGetChild(0);
+		SOCLNode params = (SOCLNode) member2.jjtGetChild(1);
+		assertEquals(SOCLParserTreeConstants.JJTIDENTIFIER, ident.getId());
+		assertEquals(SOCLParserTreeConstants.JJTPARAMETERS, params.getId());
+		
+		assertEquals(2, params.jjtGetNumChildren());
+		SOCLNode num = (SOCLNode) params.jjtGetChild(0);
+		SOCLNode str = (SOCLNode) params.jjtGetChild(1);
+		assertEquals(SOCLParserTreeConstants.JJTINTEGERCONSTANT, num.getId());
+		assertEquals(SOCLParserTreeConstants.JJTSTRINGCONSTANT, str.getId());
+		assertEquals(123, num.jjtGetValue());
+		assertEquals("str", str.jjtGetValue());
+		
+	}
+	
+	@Test
+	public void returnTest() throws ParseException {
+		SOCLParser parser = new SOCLParser("return true;");
+		
+
+		SOCLNode root = parser.parse();
+		//root.dump("");
+		
+		assertNotNull(root);
+		assertEquals(1, root.jjtGetNumChildren());
+		
+		SOCLNode member = (SOCLNode) root.jjtGetChild(0);
+		assertEquals(SOCLParserTreeConstants.JJTRETURNSTATEMENT, member.getId());
+		assertEquals(1, member.jjtGetNumChildren());
+		
+		SOCLNode obj = (SOCLNode) member.jjtGetChild(0);
+		
+		assertEquals(SOCLParserTreeConstants.JJTLOGICCONSTANT, obj.getId());
+		assertEquals(true, obj.jjtGetValue());
+		
+	}
+	
+	
 
 }
