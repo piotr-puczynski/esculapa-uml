64a5bb62fc55bbfa47f9d66f6073f2d6c7ecc7c5
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2012-02-16 10:54:24 +0100

    importing primitive types

--- dk.dtu.imm.esculapauml.core/src/dk/dtu/imm/esculapauml/core/executors/OpaqueBehaviorExecutor.java
+++ dk.dtu.imm.esculapauml.core/src/dk/dtu/imm/esculapauml/core/executors/OpaqueBehaviorExecutor.java
@@ -11,12 +11,30 @@
  ****************************************************************************/
 package dk.dtu.imm.esculapauml.core.executors;
 
+import static ch.lambdaj.Lambda.filter;
+import static ch.lambdaj.Lambda.having;
+import static ch.lambdaj.Lambda.on;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+
 import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.uml2.uml.ElementImport;
 import org.eclipse.uml2.uml.InstanceSpecification;
 import org.eclipse.uml2.uml.LiteralBoolean;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.PackageableElement;
+import org.eclipse.uml2.uml.PrimitiveType;
 import org.eclipse.uml2.uml.UMLFactory;
+import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.ValueSpecification;
+import org.eclipse.uml2.uml.VisibilityKind;
+import org.eclipse.uml2.uml.resource.UMLResource;
 
 import dk.dtu.imm.esculapauml.core.checkers.BehaviorChecker;
 import dk.dtu.imm.esculapauml.core.sal.parser.ParseException;
@@ -73,36 +91,115 @@ public class OpaqueBehaviorExecutor extends AbstractInstanceExecutor<BehaviorChe
 		}
 	}
 
+	/**
+	 * Executes a syntax tree in root.
+	 * 
+	 */
 	public void execute() {
 		for (int i = 0; i < root.jjtGetNumChildren() && !checker.hasErrors(); ++i) {
 			executeNode(root.getChild(i));
 		}
 	}
 
+	/**
+	 * Executes first level nodes.
+	 * @param node
+	 */
 	protected void executeNode(SALNode node) {
 		switch (node.getId()) {
 		case SALParserTreeConstants.JJTREPLYSTATEMENT:
 			executeReply(node);
 			break;
 		default:
-			checker.addOtherProblem(Diagnostic.ERROR, "[SAL] Statement not allowed here:  " + node.toString(), owner);
+			evaluateExpression(node);
+			// checker.addOtherProblem(Diagnostic.ERROR,
+			// "[SAL] Statement not allowed here:  " + node.toString(), owner);
 		}
 	}
 
+	/**
+	 * Executes reply.
+	 * Reply result is written to local variable and then it is possible to get it.
+	 * @param node
+	 */
 	protected void executeReply(SALNode node) {
 		reply = evaluateExpression(node.getChild(0));
 	}
 
+	/**
+	 * Evaluates any kind of expression.
+	 * @param node
+	 * @return
+	 */
 	protected ValueSpecification evaluateExpression(SALNode node) {
 		ValueSpecification result = null;
 		switch (node.getId()) {
 		case SALParserTreeConstants.JJTLOGICCONSTANT:
-			LiteralBoolean boolResult = UMLFactory.eINSTANCE.createLiteralBoolean();
-			boolResult.setValue((boolean) node.jjtGetValue());
-			result = boolResult;
+			result = evaluateLogicalConstant(node);
 			break;
 		}
 		return result;
+	}
+
+	/**
+	 * Compiles SAL Logical Constant to UML Literal.
+	 * @param node
+	 * @return
+	 */
+	protected ValueSpecification evaluateLogicalConstant(SALNode node) {
+		LiteralBoolean boolResult = UMLFactory.eINSTANCE.createLiteralBoolean();
+		PrimitiveType booleanPrimitiveType = importPrimitiveType("Boolean");
+		boolResult.setType(booleanPrimitiveType);
+		boolResult.setValue((boolean) node.jjtGetValue());
+		return boolResult;
+	}
+
+	/**
+	 * Imports UML primitive type. If no import is found already existing, the new import is automatically created.
+	 * @param name
+	 * @return
+	 */
+	protected PrimitiveType importPrimitiveType(String name) {
+		// try to find if model already import an element on package or on model
+		List<ElementImport> imports = filter(having(on(ElementImport.class).getName(), equalTo(name)), checker.getCheckedObject().getNearestPackage()
+				.getElementImports());
+		imports.addAll(filter(having(on(ElementImport.class).getName(), equalTo(name)), checker.getCheckedObject().getModel().getElementImports()));
+		int i = 0;
+		while (!imports.isEmpty()) {
+			ElementImport ei = imports.get(i);
+			PackageableElement pe = ei.getImportedElement();
+			if (pe instanceof PrimitiveType) {
+				if (pe.getQualifiedName().equals("UMLPrimitiveTypes::" + name)) {
+					return (PrimitiveType) pe;
+				}
+			}
+			imports.remove(i++);
+		}
+		// if we are here, no good import was found, we need to import ourself
+		PrimitiveType primitiveType = null;
+
+		Model umlLibrary = (Model) loadLibrary(URI.createURI(UMLResource.UML_PRIMITIVE_TYPES_LIBRARY_URI));
+
+		primitiveType = (PrimitiveType) umlLibrary.getOwnedType(name);
+
+		checker.getSystemState().addGeneratedElement(
+				checker.getCheckedObject().getNearestPackage().createElementImport(primitiveType, VisibilityKind.PUBLIC_LITERAL));
+
+		return primitiveType;
+	}
+
+	/**
+	 * Loads library in the same resource set as for checked object.
+	 * @param uri
+	 * @return
+	 */
+	protected org.eclipse.uml2.uml.Package loadLibrary(URI uri) {
+		org.eclipse.uml2.uml.Package package_ = null;
+		Resource eResource = checker.getCheckedObject().getNearestPackage().eResource();
+		ResourceSet resourceSet = eResource == null ? null : eResource.getResourceSet();
+		Resource resource = resourceSet.getResource(uri, true);
+		package_ = (org.eclipse.uml2.uml.Package) EcoreUtil.getObjectByType(resource.getContents(), UMLPackage.Literals.PACKAGE);
+		return package_;
 	}
 
 }
