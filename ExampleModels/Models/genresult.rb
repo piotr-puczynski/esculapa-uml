2f6fd541219a3e4a43c186a41078999250328660
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2012-03-08 14:10:50 +0100

    script of generation of result improved by adding git reverting

--- ExampleModels/Models/genresult.rb
+++ ExampleModels/Models/genresult.rb
@@ -21,9 +21,17 @@ ARGV.each do|arg|
 		uml.close
 		umlResult.close
 		warn "Removed #{counter} annotations from #{RESULTPATH}#{filename}.uml"
+		#revert changes in original file
+		if system("git checkout -- \"#{filename}.uml\"")
+			warn "Git checkout #{filename}.uml sucessful"
+		end
 	end
 	if File.exist? "#{filename}.umldi"
 		FileUtils.cp "#{filename}.umldi", "#{RESULTPATH}#{filename}.umldi"
 		warn "#{RESULTPATH}#{filename}.umldi was copied"
+		#revert changes in original file
+		if system("git checkout -- \"#{filename}.umldi\"")
+			warn "Git checkout #{filename}.umldi sucessful"
+		end
 	end
 end
