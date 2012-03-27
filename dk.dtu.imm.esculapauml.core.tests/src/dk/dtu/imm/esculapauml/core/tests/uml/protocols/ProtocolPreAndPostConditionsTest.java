a85695e078c49b1a299530bd1fd85664663624b2
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2012-03-27 16:08:42 +0200

    protocol with pre and post conditions successful test

---
+++ dk.dtu.imm.esculapauml.core.tests/src/dk/dtu/imm/esculapauml/core/tests/uml/protocols/ProtocolPreAndPostConditionsTest.java
@@ -0,0 +1,50 @@
+/******************************************************************************
+ * Copyright (c) 2011, 2012 Piotr J. Puczynski (DTU Informatics).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Piotr J. Puczynski (DTU Informatics) - initial API and implementation 
+ *    
+ ****************************************************************************/
+package dk.dtu.imm.esculapauml.core.tests.uml.protocols;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.uml2.uml.Interaction;
+import org.junit.Test;
+
+import dk.dtu.imm.esculapauml.core.checkers.UseCaseChecker;
+import dk.dtu.imm.esculapauml.core.tests.uml.LoggingTest;
+import dk.dtu.imm.esculapauml.core.tests.utils.TestUtils;
+
+/**
+ * Tests successful scenario when the protocol order is valid and protocol has
+ * pre- and post-conditions.
+ * 
+ * @author Piotr. J. Puczynski (piotr.puczynski)
+ * 
+ */
+public class ProtocolPreAndPostConditionsTest extends LoggingTest {
+
+	private Resource model = TestUtils.getUMLResource("ProtocolPreAndPostConditions.uml");
+	private Resource referenceModel = TestUtils.getUMLResource("results/ProtocolPreAndPostConditions.uml");
+
+	@Test
+	public void protocolPreAndPostConditions() throws InterruptedException {
+		Interaction interaction = TestUtils.getInteraction(model, "UseCase1Detail");
+		assertNotNull(interaction);
+		UseCaseChecker checker = new UseCaseChecker(interaction);
+		checker.check();
+		Diagnostic diagnostics = checker.getDiagnostics();
+		// there is no error
+		assertEquals(Diagnostic.OK, diagnostics.getSeverity());
+		// models have no differences
+		assertTrue(TestUtils.modelsHaveNoDifferences(model, referenceModel));
+	}
+
+}
