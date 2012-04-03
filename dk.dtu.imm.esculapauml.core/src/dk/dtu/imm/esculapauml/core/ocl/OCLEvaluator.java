dfaf291c1ad329bb13c659c3845834da6d98f7c7
Piotr J. Puczynski <piotr.puczynski@gmail.com>
2012-04-03 15:25:45 +0200

    improved oclevaluator errors

--- dk.dtu.imm.esculapauml.core/src/dk/dtu/imm/esculapauml/core/ocl/OCLEvaluator.java
+++ dk.dtu.imm.esculapauml.core/src/dk/dtu/imm/esculapauml/core/ocl/OCLEvaluator.java
@@ -74,7 +74,13 @@ public class OCLEvaluator {
 		}
 		@SuppressWarnings("unchecked")
 		Object result = myOCL.evaluate(context, (OCLExpression<Classifier>) oclConstraint);
-		if (null == result || myOCL.isInvalid(result)) {
+		if (null == result) {
+			checker.addOtherProblem(Diagnostic.ERROR, "Evaluation of '" + expression + "' in context of '" + context.getLabel() + "' returned no value.",
+					errorContext);
+			hasErrors = true;
+			return null;
+		}
+		if (myOCL.isInvalid(result)) {
 			checker.addOtherProblem(Diagnostic.ERROR, "Evaluation of '" + expression + "' in context of '" + context.getLabel() + "' returned invalid value.",
 					errorContext);
 			hasErrors = true;
